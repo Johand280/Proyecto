508aa662a63218505b1d9e9d8aa229cbd3e47dc0
Johand280 <johandavidmosqueraorejuela@gmail.com>
2024-11-15 18:56:08 -0500

    Correciones

--- usuarios.js
+++ usuarios.js
@@ -1,27 +1,23 @@
 let indice = 0;
-
     function cambiarImagen(direccion) {
         const imagenes = document.querySelector('.imagenes');
         const totalImagenes = document.querySelectorAll('.imagen').length;
         indice += direccion;
-
         if (indice < 0) {
             indice = totalImagenes - 1;
         } else if (indice >= totalImagenes) {
             indice = 0;
         }
-
         const desplazamiento = -indice * 100;
         imagenes.style.transform = `translateX(${desplazamiento}%)`;
     }
-
-const nav = document.querySelector('.nav');
-const abrir = document.querySelector('.abrir_menu');
-const cerrar = document.querySelector('.cerrar_menu');
+const nav = document.querySelector("#Nav");
+const abrir = document.querySelector("#Abrir");
+const cerrar = document.querySelector("#Cerrar");
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
+    nav.classList.add(" visible");
 })
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
+    nav.classList.remove(" visible");
 })
