b3e9e95db961e4ddd0596263ea3c4c7650b4be23
Johand280 <johandavidmosqueraorejuela@gmail.com>
2024-11-15 11:59:32 -0500

    Carrusel2

--- usuarios.js
+++ usuarios.js
@@ -1,1 +1,27 @@
-////de momento nada///
+let indice = 0;
+
+    function cambiarImagen(direccion) {
+        const imagenes = document.querySelector('.imagenes');
+        const totalImagenes = document.querySelectorAll('.imagen').length;
+        indice += direccion;
+
+        if (indice < 0) {
+            indice = totalImagenes - 1;
+        } else if (indice >= totalImagenes) {
+            indice = 0;
+        }
+
+        const desplazamiento = -indice * 100;
+        imagenes.style.transform = `translateX(${desplazamiento}%)`;
+    }
+
+const nav = document.querySelector('.nav');
+const abrir = document.querySelector('.abrir_menu');
+const cerrar = document.querySelector('.cerrar_menu');
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
